2272c28e6d7ffad225945088a670c6e895ec8be2
Phoenix Nemo <mrphoenixlzx@gmail.com>
2013-06-22 16:54:11 +0800

    fixed username length

--- controllers/sign.js
+++ controllers/sign.js
@@ -30,8 +30,8 @@ exports.signup = function (req, res, next) {
     return;
   }
 
-  if (name.length < 5) {
-    res.render('sign/signup', {error: '用户名至少需要5个字符。', name: name, email: email});
+  if (name.length < 2) {
+    res.render('sign/signup', {error: '用户名至少需要2个字符。', name: name, email: email});
     return;
   }
 
