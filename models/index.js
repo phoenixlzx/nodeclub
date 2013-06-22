e9e9c8815299ec52f4fced16e60f6f74e431d301
Phoenix Nemo <mrphoenixlzx@gmail.com>
2013-06-22 15:18:12 +0800

    topic id fix test 3, thanks to snoopyxd

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var config = require('../config').config;
 
-mongoose.connect(config.db, function (err) {
+mongoose.connect(config.db, {db:{safe:true}}, function (err) {
   if (err) {
     console.error('connect to %s error: ', config.db, err.message);
     process.exit(1);
